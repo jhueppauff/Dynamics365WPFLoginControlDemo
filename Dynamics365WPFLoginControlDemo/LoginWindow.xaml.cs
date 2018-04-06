3558dac1eeecaf1b711e8d3a3ba6cf64265b50b0
Julian Hüpppauff <julian@hueppauff.info>
2018-04-07 00:44:37 +0200

    Added events

--- Dynamics365WPFLoginControlDemo/LoginWindow.xaml.cs
+++ Dynamics365WPFLoginControlDemo/LoginWindow.xaml.cs
@@ -1,27 +1,159 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
-namespace Dynamics365WPFLoginControlDemo
+﻿namespace Dynamics365WPFLoginControlDemo
 {
+    using Microsoft.Xrm.Tooling.Connector;
+    using Microsoft.Xrm.Tooling.CrmConnectControl;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Shapes;
+    using System.Windows.Threading;
+
     /// <summary>
     /// Interaction logic for LoginWindow.xaml
     /// </summary>
     public partial class LoginWindow : Window
     {
+        #region variables
+
+        /// <summary>
+        /// Crm Service Client
+        /// </summary>
+        private CrmServiceClient CrmServiceClient = null;
+
+        /// <summary>
+        /// flag to determine if ther is a connection
+        /// </summary>
+        private bool connectionState = false;
+
+        /// <summary>
+        /// Crm Connection Manager
+        /// </summary>
+        private CrmConnectionManager crmConnectionManager = null;
+
+        /// <summary>
+        /// Used to reset the ui without reopening
+        /// </summary>
+        private bool resetUiFlag = false;
+        #endregion
+
+        #region properties
+
+        /// <summary>
+        /// Gets the CrmConnectionManager
+        /// </summary>
+        public CrmConnectionManager CrmConnectionManager { get { return crmConnectionManager; } }
+        #endregion
+
+        #region event
+
+        /// <summary>
+        /// Raised when the connection was established
+        /// </summary>
+        public event EventHandler ConnectionToCrmCompleted;
+        #endregion
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoginWindow"/> class.
+        /// </summary>
         public LoginWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
+
+        /// <summary>
+        /// Raised when the window loads 
+        /// </summary>
+        /// <param name="sender">The Sender</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.connectionState = false;
+
+            // Initialize the connection Manager
+            this.crmConnectionManager = new CrmConnectionManager
+            {
+                ParentControl = this.CrmLoginControl,
+                UseUserLocalDirectoryForConfigStore = true
+            };
+
+            // Configure the Crm Control
+            this.CrmLoginControl.SetGlobalStoreAccess(crmConnectionManager);
+            this.CrmLoginControl.SetControlMode(ServerLoginConfigCtrlMode.FullLoginPanel);
+
+            // Event registration
+            this.CrmLoginControl.ConnectionCheckBegining += new EventHandler(CrmLoginControl_ConnectionCheckStarted);
+            this.CrmLoginControl.ConnectErrorEvent += new EventHandler<ConnectErrorEventArgs>(CrmLoginControl_ConnectionErrorRaised);
+            this.CrmLoginControl.ConnectionStatusEvent += new EventHandler<ConnectStatusEventArgs>(CrmLoginControl_ConnectionStatusEventRaised);
+            this.CrmLoginControl.UserCancelClicked += new EventHandler(CrmLoginControl_UserCancelClicked);
+
+            // Check if an auto login is possible
+            if (!this.crmConnectionManager.RequireUserLogin())
+            {
+#pragma warning disable S1066 // Collapsible "if" statements should be merged
+                if (MessageBox.Show("There are already Credentials saved in your configuration\nDo you want to load those?\nYes to Auto Login or No to Reset Credentials", "Auto Login possible", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+#pragma warning restore S1066 // Collapsible "if" statements should be merged
+                {
+                    // Credentials are cached
+                    this.CrmLoginControl.IsEnabled = false;
+
+                    this.crmConnectionManager.ServerConnectionStatusUpdate += new EventHandler<ServerConnectStatusEventArgs>(crmConnectionManager_ServerConnectionStatusUpdateRaised);
+                    this.crmConnectionManager.ConnectionCheckComplete += new EventHandler<ServerConnectStatusEventArgs>(crmConnectionManager_ConnectionCheckCompleted);
+
+                    this.crmConnectionManager.ConnectToServerCheck();
+
+                    // Show message
+                    this.CrmLoginControl.ShowMessageGrid();
+                }
+            }
+
+        }
+
+        #region events
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender">The Sender</param>
+        /// <param name="e">The <see cref="ServerConnectStatusEventArgs"/> instance containing the event data.</param>
+        private void crmConnectionManager_ConnectionCheckCompleted(object sender, ServerConnectStatusEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void crmConnectionManager_ServerConnectionStatusUpdateRaised(object sender, ServerConnectStatusEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CrmLoginControl_UserCancelClicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CrmLoginControl_ConnectionStatusEventRaised(object sender, ConnectStatusEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CrmLoginControl_ConnectionErrorRaised(object sender, ConnectErrorEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CrmLoginControl_ConnectionCheckStarted(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
     }
 }
