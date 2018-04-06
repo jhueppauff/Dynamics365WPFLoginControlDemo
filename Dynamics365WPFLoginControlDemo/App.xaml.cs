f14a99586f13f69b2faabceaa67692d75868482c
Julian Hüpppauff <julian@hueppauff.info>
2018-04-07 00:50:29 +0200

    removed unused usings

--- Dynamics365WPFLoginControlDemo/App.xaml.cs
+++ Dynamics365WPFLoginControlDemo/App.xaml.cs
@@ -5,16 +5,10 @@
 // For licence details visit https://github.com/jhueppauff/Dynamics365WPFLoginControlDemo/blob/master/LICENSE
 //-----------------------------------------------------------------------
 
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
 namespace Dynamics365WPFLoginControlDemo
 {
+    using System.Windows;
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
