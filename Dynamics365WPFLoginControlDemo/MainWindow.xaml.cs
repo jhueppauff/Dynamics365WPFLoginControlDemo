796770ea5d01a300d5e65511dc99b90055e82dbe
Julian Hüpppauff <julian@hueppauff.info>
2018-04-07 00:45:47 +0200

    moved namespace, added summary

--- Dynamics365WPFLoginControlDemo/MainWindow.xaml.cs
+++ Dynamics365WPFLoginControlDemo/MainWindow.xaml.cs
@@ -1,21 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace Dynamics365WPFLoginControlDemo
+﻿namespace Dynamics365WPFLoginControlDemo
 {
-    using Microsoft.Xrm.Tooling.CrmConnectControl;
+    using System.Windows;
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -30,6 +15,11 @@ namespace Dynamics365WPFLoginControlDemo
             this.InitializeComponent();
         }
 
+        /// <summary>
+        /// Will open the Login Window
+        /// </summary>
+        /// <param name="sender">The Sender</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             LoginWindow loginWindow = new LoginWindow();
